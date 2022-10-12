9e05386a3f652e3fbee86e1ad7b998a0a96326ba
Jarosz Léa <jarosz1u@etu.univ-lorraine.fr>
2022-10-12 17:36:20 +0200

    ajout du text de fin + le nb de coup effectuer

--- PlusOuMoinsJS/PlusOuMoins.js
+++ PlusOuMoinsJS/PlusOuMoins.js
@@ -2,15 +2,20 @@ let random = Math.floor(Math.random() * 99) + 1;
 
 console.log(random);
 
-let nb = prompt("entrer un nombre entre 1 et 99");
+let nb = prompt("Entrer un nombre entre 1 et 99");
 
 console.log(nb);
 
+let count = 1;
+
 while(nb != random){
     if(nb > random){
-        nb = prompt("c'est moins");
+        nb = prompt("C'est moins");
     }else{
-        nb = prompt("c'est plus");
+        nb = prompt("C'est plus");
     }
+    count ++;
 }
+
+alert("Bravo, la valeur a trouve etait "+nb+" vous l'avez trouver en "+count+" coup");
     
