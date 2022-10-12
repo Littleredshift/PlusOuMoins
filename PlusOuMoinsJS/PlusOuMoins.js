8355f988f134a2ac522d045f650f5cc39a6f9e94
Lloyd <leblanc71u@etu.univ-lorraine.fr>
2022-10-12 17:18:38 +0200

    Update PlusOuMoins.js

--- PlusOuMoinsJS/PlusOuMoins.js
+++ PlusOuMoinsJS/PlusOuMoins.js
@@ -2,5 +2,15 @@ let random = Math.floor(Math.random() * 99) + 1;
 
 console.log(random);
 
-console.log("entrer un nombre entre 1 et 99");
-let nb = prompt();
+let nb = prompt("entrer un nombre entre 1 et 99");
+
+console.log(nb);
+
+while(nb != random){
+    if(nb > random){
+        nb = prompt("c'est moins");
+    }else{
+        nb = prompt("c'est plus");
+    }
+}
+    
