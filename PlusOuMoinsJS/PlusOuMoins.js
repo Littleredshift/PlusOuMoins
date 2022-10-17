3ce11bdb5dee35ecc1b98e8155885c12fdb3af11
Lloyd <leblanc71u@etu.univ-lorraine.fr>
2022-10-17 17:58:24 +0200

    ajout highScore
    ajout du highScore , trie des highScore , affichage de la place dans les highScore

--- PlusOuMoinsJS/PlusOuMoins.js
+++ PlusOuMoinsJS/PlusOuMoins.js
@@ -1,3 +1,14 @@
+function compareScore(a,b){
+    return a.score - b.score;
+}
+
+function displayHighScore(array){
+    array.forEach(function(element){
+        console.log(element.name + " : " + element.score);
+    });
+}
+
+
 let random = Math.floor(Math.random() * 99) + 1; 
 
 console.log(random);
@@ -19,5 +30,20 @@ while(nb != random){
 
 alert("Bravo, la valeur a trouve etait "+nb+" vous l'avez trouver en "+count+" coup");
 
-let name = prompt("Veillez entrer votre nom");
-    
+
+let user = {};
+user.name = prompt("Veillez entrer votre nom");
+user.score = count;
+
+if(window.localStorage.getItem('higscores') !== null){
+    let highscores = JSON.parse(window.localStorage.getItem('higscores'));
+    highscores.push(user);
+    highscores.sort(compareScore);
+    console.log("vous êtes à la place numero "+highscores.indexOf(user) + " du classement avec un score de "+user.score); 
+    window.localStorage.setItem('higscores', JSON.stringify(highscores));
+}else{
+    let highscores = [];
+    highscores.push(user);
+    window.localStorage.setItem('higscores', JSON.stringify(highscores));
+}
+
